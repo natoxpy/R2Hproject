cd6d14f286122496c279026c709674d87fdb2948
natoxpy <natoxpy@gmail.com>
2023-12-03 17:34:37 -0500

    Auto redirect from home page to aboutme

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+"use client";
 import Homepage from "./aboutme/page";
 import Loading from "./loading";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  router.push("/aboutme");
   return (
     <>
       <Homepage />
